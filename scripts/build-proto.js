83ea3edd1815c20628521df4f03e4835b1074949
Denis <carriere.denis@gmail.com>
2018-02-02 11:35:11 -0500

    move build-proto

--- scripts/build-proto.js
+++ scripts/build-proto.js
@@ -7,8 +7,8 @@ const { pbjs, pbts } = require('protobufjs/cli')
 glob.sync(path.join(__dirname, '..', 'sharedstreets-ref-system', 'proto', '*.proto')).forEach(filepath => {
   // Filepaths
   const {name, base} = path.parse(filepath)
-  const js = path.join(__dirname, name + '.js')
-  const dts = path.join(__dirname, name + '.dts')
+  const js = path.join(__dirname, '..', 'proto', name + '.js')
+  const dts = path.join(__dirname, '..', 'proto', name + '.d.ts')
 
   // Create ProtobufJS Static Javascript
   pbjs.main(['-t', 'static-module', '-w', 'commonjs', '-o', js, filepath])
