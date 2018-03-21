65f676fa4fd12c75ff7ad2a98a8838e74f741415
Denis <carriere.denis@gmail.com>
2018-03-21 16:42:10 -0400

    Update test fixtures to planet-180312

--- test.ts
+++ test.ts
@@ -9,14 +9,15 @@ import write from "write-json-file";
 import * as sharedstreetsPbf from "./";
 
 const layers: Layer[] = ["geometry", "intersection", "metadata", "reference"];
-const tile = [12, 1186, 1466];
-const [z, x, y] = tile;
+const tile = [1186, 1466, 12];
+const [x, y, z] = tile;
+const domain = "tiles.sharedstreets.io/planet-180312";
 
 // Download Tile
 // Set Environment Variable `export DOWNLOAD=true` to regenerate
 if (process.env.DOWNLOAD) {
   layers.forEach((layer) => {
-    downloadTile(tile, layer).then((data) => {
+    downloadTile(tile, layer, {domain}).then((data) => {
       const filepath = path.join(__dirname, "test", "in", `${z}-${x}-${y}.${layer}.pbf`);
       fs.writeFileSync(filepath, data);
     });
@@ -27,14 +28,13 @@ const pbf: any = sharedstreetsPbf;
 layers.forEach((layer) => {
   test("sharedstreets-pbf -- " + layer, (t) => {
     const filepath = path.join(__dirname, "test", "in", `${z}-${x}-${y}.${layer}.pbf`);
-    const fileout = path.join(__dirname, "test", "out", `${z}-${x}-${y}.${layer}.json`);
+    const outfile = path.join(__dirname, "test", "out", `${z}-${x}-${y}.${layer}.json`);
     const {name, base} = path.parse(filepath);
     const buffer = fs.readFileSync(filepath);
     const result = pbf[layer](buffer);
 
     // Save Result
     // Set Environment Variable `export REGEN=true` to regenerate
-    const outfile = filepath.replace(path.join("test", "in", base), path.join("test", "out", name + ".json"));
     if (process.env.REGEN) { write.sync(outfile, result); }
     t.deepEqual(result, load.sync(outfile), name);
     t.end();
